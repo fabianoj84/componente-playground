d92f8b639907e02eb35302c9ad8748c8eb4d354e
Fabiano Jose <fabianoj84@gmail.com>
2025-08-11 15:03:06 -0300

    feat: cria componente Navbar (estrutura)

---
+++ src/componets/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">Minha Marca</div>
+      <ul className="navbar__links">
+        <li>
+          <a href="#">Início</a>
+        </li>
+        <li>
+          <a href="#">Serviço</a>
+        </li>
+        <li>
+          <a href="#">Contato</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
