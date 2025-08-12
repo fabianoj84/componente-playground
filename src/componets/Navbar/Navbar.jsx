6129223716be5437793b51311e13bb4f9b6162f2
Fabiano Jose <fabianoj84@gmail.com>
2025-08-12 15:25:35 -0300

    fix: fixação da Navbar no topo da página

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -1,18 +1,22 @@
+import "./Navbar.css";
+
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar__logo">Minha Marca</div>
-      <ul className="navbar__links">
-        <li>
-          <a href="#">Início</a>
-        </li>
-        <li>
-          <a href="#">Serviço</a>
-        </li>
-        <li>
-          <a href="#">Contato</a>
-        </li>
-      </ul>
+      <div className="navbar__inner">
+        <div className="navbar__logo">Minha Marca</div>
+        <ul className="navbar__links">
+          <li>
+            <a href="#">Início</a>
+          </li>
+          <li>
+            <a href="#">Serviço</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
